b6abdf4105e9eee6d85cb770ddd041e8cb5bf4dc
Divyank Dhadi <divyank004@gmail.com>
2025-06-24 16:36:29 +0200

    add HMR, update auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as argon2 from 'argon2';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Injectable()
 export class AuthService {
@@ -17,13 +18,17 @@ export class AuthService {
     const user = await this.userService.findOne(username);
 
     if (!user) {
-      throw new UnauthorizedException('Invalid credentials');
+      throw new UnauthorizedException('User does not exist. Please register.');
     }
+
     const verified: boolean = await argon2.verify(user.password, password);
+
     if (!verified) {
       throw new UnauthorizedException('Invalid credentials');
     }
+
     const payload = { sub: user.id, username: user.username };
+
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const swaggerConfig = new DocumentBuilder()
@@ -14,5 +16,13 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (module.hot) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    module.hot.accept();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    module.hot.dispose(() => app.close());
+  }
 }
 void bootstrap();
